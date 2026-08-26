f464f9e30ed41d4012bc8f802bf5d22ca9ba9e50
spacexmerlin <naod70@googlemail.com>
2026-08-26 16:03:29 +0200

    Add files via upload

---
+++ js/shows-data.js
@@ -0,0 +1,56 @@
+/**
+ * Tour-/Showdaten für Simon Mann.
+ * Neuen Termin hinzufügen: einfach ein weiteres Objekt in das Array einfügen.
+ * date: "JJJJ-MM-TT" (ISO-Format), ticketUrl: leer lassen ("") wenn noch keine Tickets online sind.
+ * soldOut: true setzt den Status auf "Ausverkauft".
+ */
+const SHOWS = [
+  {
+    date: "2026-10-08",
+    city: "Hamburg",
+    venue: "Amandastr. 58",
+    time: "20:00 – 22:00 Uhr",
+    ticketUrl: "",
+    soldOut: false
+  },
+  {
+    date: "2026-10-13",
+    city: "Frankfurt am Main",
+    venue: "Weißfrauenstraße 2-8",
+    time: "20:00 – 22:00 Uhr",
+    ticketUrl: "",
+    soldOut: false
+  },
+  {
+    date: "2026-10-20",
+    city: "Düsseldorf",
+    venue: "Oststraße 147",
+    time: "20:00 – 22:00 Uhr",
+    ticketUrl: "",
+    soldOut: false
+  },
+  {
+    date: "2026-10-25",
+    city: "München",
+    venue: "Feilitzschstraße 12",
+    time: "20:00 – 22:00 Uhr",
+    ticketUrl: "",
+    soldOut: false
+  },
+  {
+    date: "2026-10-29",
+    city: "Leipzig",
+    venue: "Barfußgäßchen 12",
+    time: "20:00 – 22:00 Uhr",
+    ticketUrl: "",
+    soldOut: false
+  },
+  {
+    date: "2026-11-03",
+    city: "Berlin",
+    venue: "Oranienburger Str. 67",
+    time: "20:00 – 22:00 Uhr",
+    ticketUrl: "",
+    soldOut: false
+  }
+];
